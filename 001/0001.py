104161e7617184d99cf47f1dbe2ab01afd029a68
mingsi283 <liaomingsi@gmail.com>
2021-03-14 18:58:59 +0800

    11

--- 001/0001.py
+++ 001/0001.py
@@ -1,9 +1,4 @@
 print("hello world")
-<<<<<<< HEAD
-for i in range(10):
-    print("hello")
-    print("你好")
-=======
 import  tensorflow as tf
 a = tf.constant(1.2)
 b=tf.constant(2.0)
@@ -12,4 +7,3 @@ print(c)
 print("修改了一下下")
 print("hi")
 print(("errt"))
->>>>>>> bf1e52c8622c0a340ca2433d699472d169f0a50a
